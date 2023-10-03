d1dd89056cf8abd5428bc0f07ae54ecbddc03580
AlokMahapatra26 <alokmahapatra2604@gmail.com>
2023-10-03 23:59:18 +0530

    props added

---
+++ 01_basics/props/src/App.jsx
@@ -0,0 +1,15 @@
+
+import Card from "./Card"
+
+function App() {
+  
+
+  return (
+    <>
+    
+    <Card des="hello there its a description" name="Alok" imgUrl="https://scontent.fstv15-1.fna.fbcdn.net/v/t1.6435-9/86346554_100417424887752_4975132340062781440_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=x0ubEkeq2gkAX_vYnsg&_nc_ht=scontent.fstv15-1.fna&oh=00_AfC_z1L6-WehqYkty4DC7PKjB9HKzVo637tqgPVQMPZj5g&oe=654298DB"/>
+    </>
+  )
+}
+
+export default App
