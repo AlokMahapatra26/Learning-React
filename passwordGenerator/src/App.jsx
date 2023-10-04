255d586a391fcd52c5cedd27fd3c92679e6b2e18
AlokMahapatra26 <alokmahapatra2604@gmail.com>
2023-10-05 02:47:21 +0530

    password generator Done

--- passwordGenerator/src/App.jsx
+++ passwordGenerator/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useState } from 'react'
+import React, { useCallback, useEffect, useState, useRef } from 'react'
 
 
 export default function App() {
@@ -8,6 +8,10 @@ export default function App() {
     const [charAllowed , setCharAllowed] = useState(false);
     const [password , setPassword] = useState("");
 
+    //useRef hook
+    const passwordRef = useRef(null);
+
+
     const passwordGenerator = useCallback(() => {
         let pass = ""
         let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
@@ -16,25 +20,34 @@ export default function App() {
 
         for(let i = 1 ; i <= length; i++){
             let char  =  Math.floor(Math.random()*str.length+1);
-            pass = str.charAt(char);
+            pass = pass + str.charAt(char);
         }
         
         setPassword(pass);
 
     } , [length , numberAllowed , charAllowed , setPassword])
 
+    const copyPasswordToClipBoard = useCallback(()=>{
+        passwordRef.current.select();
+        passwordRef.current.setSelectionRange(0,16)
+        window.navigator.clipboard.writeText(password)}, [password])
+
+    useEffect(()=>{
+        passwordGenerator();
+    },[length , numberAllowed , charAllowed , ]);
+
   return (
     <>
     <h1 className='text-center mt-5 text-2xl'>Password Generator</h1>
-    <div className='w-full max-w-md mx-auto shadow-md rounded-lg px-4 my-8 '>{password}</div>
+    
     <div className='order mt-8 p-2 rounded outline-none mx-auto w-full text-center'> 
-        <input type="text" value={password} className='border mt-8 p-2  outline-none  mx-auto' readOnly/>
-        <button className='p-2 bg-blue-200 border '>copy</button>
+        <input type="text" value={password} className='border mt-8 p-2  outline-none  mx-auto' readOnly ref={passwordRef}/>
+        <button onClick={copyPasswordToClipBoard} className='p-2 bg-blue-200 border '>copy</button>
     </div>
 
     <div className='flex justify-between max-w-md mx-auto mt-4 items-center'>
     <div>
-        <input type="range" className='text-center ' min={6} max={18} value={length} onChange={(e)=>{setLength(e.target.value)}}/>
+        <input type="range"  className='text-center ' min={6} max={18} value={length} onChange={(e)=>{setLength(e.target.value)}}/>
         <label>Length : {length}</label>
     </div>
     <div>
